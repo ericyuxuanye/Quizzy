da786e1cf704e6cb837cf3d93ac8d0226c5c92d7
Eric Ye <yuxuan.smiley@gmail.com>
2021-05-23 12:34:42 -0700

    removed unnecessary public modifiers

--- src/main/java/App/QuizQuestion.java
+++ src/main/java/App/QuizQuestion.java
@@ -11,21 +11,21 @@ public interface QuizQuestion extends Serializable {
     /**
      * Gets the panel to show the quiz question
      */
-    public JPanel getPanel();
+    JPanel getPanel();
 
     /**
      * Gets the panel with JTextFields so the user can
      * edit the current question
      */
-    public JPanel getPanelEditable();
+    JPanel getPanelEditable();
 
     /**
      * Returns whether the user selected the correct answer
      */
-    public boolean isCorrect() throws EmptyQuestionException;
+    boolean isCorrect() throws EmptyQuestionException;
 
     /**
      * Saves the correct answer in edit pane
      */
-    public void save() throws EmptyQuestionException;
+    void save() throws EmptyQuestionException;
 }
