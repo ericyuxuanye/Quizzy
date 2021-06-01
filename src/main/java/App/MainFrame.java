63bd3b3f9c6852d863b187b49c2939d7980b6ec7
Eric Ye <yuxuan.smiley@gmail.com>
2021-06-01 10:17:44 -0700

    Organize MainFrame constructor better

--- src/main/java/App/MainFrame.java
+++ src/main/java/App/MainFrame.java
@@ -6,18 +6,15 @@ import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 public class MainFrame extends JFrame {
-    private final HomeScreen hs;
-    private final QuizEditScreen quizEdit;
-    private final TakeQuizScreen takeQuiz;
+    private final HomeScreen hs = new HomeScreen();
+    private final QuizEditScreen quizEdit = new QuizEditScreen();
+    private final TakeQuizScreen takeQuiz = new TakeQuizScreen();
 
     public MainFrame() {
         super("Quizzy");
         setMinimumSize(new Dimension(500, 300));
         setPreferredSize(new Dimension(1000, 600));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        hs = new HomeScreen();
-        quizEdit = new QuizEditScreen();
-        takeQuiz = new TakeQuizScreen();
         setContentPane(hs);
         pack();
         setVisible(true);
